5eb01c2a6f8292eacb1944be5e284deed2f628dc
Anderson Coelho <anderson.coelho1969@gmail.com>
2025-09-15 21:51:15 -0400

    fix: install .eslint on project and standard files

--- src/domain/entities/question.ts
+++ src/domain/entities/question.ts
@@ -1,90 +1,90 @@
-import { Entity } from "@/core/entities/entity"
-import { UniqueEntityID } from "@/core/entities/unique-entity-id"
-import { Optional } from "@/core/types/optional"
-import dayjs from "dayjs"
-import { Slug } from "./value-objects/slug"
+import { Entity } from '@/core/entities/entity'
+import { UniqueEntityID } from '@/core/entities/unique-entity-id'
+import { Optional } from '@/core/types/optional'
+import dayjs from 'dayjs'
+import { Slug } from './value-objects/slug'
 
 interface QuestionProps {
-    authorId: UniqueEntityID
-    bestAnswerId?: UniqueEntityID
-    title: string
-    content: string
-    slug: Slug
-    createdAt: Date
-    updatedAt?: Date
+  authorId: UniqueEntityID
+  bestAnswerId?: UniqueEntityID
+  title: string
+  content: string
+  slug: Slug
+  createdAt: Date
+  updatedAt?: Date
 }
 
 export class Question extends Entity<QuestionProps> {
-    get authorId() {
-        return this.props.authorId
-    }
+  get authorId() {
+    return this.props.authorId
+  }
 
-    get bestAnswerId() {
-        return this.props.bestAnswerId
-    }
+  get bestAnswerId() {
+    return this.props.bestAnswerId
+  }
 
-    get title() {
-        return this.props.title
-    }
+  get title() {
+    return this.props.title
+  }
 
-    get content() {
-        return this.props.content
-    }
-    
-    get slug() {
-        return this.props.slug
-    }
+  get content() {
+    return this.props.content
+  }
 
-    get createdAt() {
-        return this.props.createdAt
-    }
+  get slug() {
+    return this.props.slug
+  }
 
-    get updatedAt() {
-        return this.props.updatedAt
-    }
+  get createdAt() {
+    return this.props.createdAt
+  }
 
-    get isNew(): boolean {
-        return dayjs().diff(this.createdAt, 'days') <= 3
-    }
+  get updatedAt() {
+    return this.props.updatedAt
+  }
 
-    get excerpt() {
-        return this.content
-            .substring(0, 120)
-            .trimEnd()
-            .concat('...')
-    }
+  get isNew(): boolean {
+    return dayjs().diff(this.createdAt, 'days') <= 3
+  }
 
-    private touch() {
-        this.props.updatedAt = new Date()
-    }
+  get excerpt() {
+    return this.content.substring(0, 120).trimEnd().concat('...')
+  }
 
-    set title(title: string) {
-        this.props.title = title
-        this.props.slug = Slug.createFromText(title)
+  private touch() {
+    this.props.updatedAt = new Date()
+  }
 
-        this.touch()
-    }
+  set title(title: string) {
+    this.props.title = title
+    this.props.slug = Slug.createFromText(title)
 
-    set content(content: string) {
-        this.props.content = content
-        this.touch()
-    }
+    this.touch()
+  }
 
-    set bestAnswerId(bestAnswerId: UniqueEntityID | undefined) {
-        this.props.bestAnswerId = bestAnswerId
-        this.touch()
-    }
+  set content(content: string) {
+    this.props.content = content
+    this.touch()
+  }
 
-    static create(
-        props: Optional<QuestionProps, 'createdAt' | 'slug'>, 
-        id?: UniqueEntityID
-    ) {
-        const question = new Question({
-            ...props,
-            slug: props.slug ?? Slug.createFromText(props.title),
-            createdAt: new Date(),
-        }, id)
+  set bestAnswerId(bestAnswerId: UniqueEntityID | undefined) {
+    this.props.bestAnswerId = bestAnswerId
+    this.touch()
+  }
 
-        return question
-    }
+  static create(
+    props: Optional<QuestionProps, 'createdAt' | 'slug'>,
+    id?: UniqueEntityID,
+  ) {
+    const question = new Question(
+      {
+        ...props,
+        slug: props.slug ?? Slug.createFromText(props.title),
+        createdAt: new Date(),
+      },
+      id,
+    )
+
+    return question
+  }
 }
--- src/domain/use-cases/answer-question.spec.ts
+++ src/domain/use-cases/answer-question.spec.ts
@@ -1,21 +1,20 @@
-import { Answer } from '../entities/answer';
-import { AnswerRepository } from '../repositories/answers-repository';
-import { AnswerQuestionUseCase } from './answer-question';
+import { Answer } from '../entities/answer'
+import { AnswerRepository } from '../repositories/answers-repository'
+import { AnswerQuestionUseCase } from './answer-question'
 
 const fakeAnswerRepository: AnswerRepository = {
-    create: async (answer: Answer) => {
-        return;
-    }
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  create: async (answer: Answer) => {},
 }
 
 test('create an answer', async () => {
-    const answerQuestion = new AnswerQuestionUseCase(fakeAnswerRepository)
+  const answerQuestion = new AnswerQuestionUseCase(fakeAnswerRepository)
 
-    const answer = await answerQuestion.execute({
-        questionId: '1',
-        instructorId: '1',
-        content: 'Nova resposta',
-    })
+  const answer = await answerQuestion.execute({
+    questionId: '1',
+    instructorId: '1',
+    content: 'Nova resposta',
+  })
 
-    expect(answer.content).toEqual('Nova resposta')
+  expect(answer.content).toEqual('Nova resposta')
 })
